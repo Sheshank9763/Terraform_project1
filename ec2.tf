7b28e5145fdce8f0e9fedc9eb165e4b9ef014815
Sheshank9763 <pandirisheshank7780@gmail.com>
2024-01-30 23:20:42 +0530

    1st commit

---
+++ ec2.tf
@@ -0,0 +1,21 @@
+resource "aws_instance" "public-ec2" {
+    ami = "ami-008fe2fc65df48dac"
+    subnet_id = aws_subnet.public1.id
+    tags = {
+      Name = "public-ec2"
+    }
+    key_name = "sheshi.pem"
+    instance_type = "t2.micro"
+    associate_public_ip_address = true
+    security_groups = [ aws_security_group.sg1.id ]
+}
+resource "aws_instance" "private-ec2" {
+    ami = "ami-008fe2fc65df48dac"
+    subnet_id = aws_subnet.public2.id
+    tags = {
+      Name = "private-ec2"
+    }
+    key_name = "sheshi.pem"
+    instance_type = "t2.micro"
+    security_groups = [aws_security_group.sg1.id]
+}
